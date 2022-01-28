463bff65d49cc562b67752be76a6ad8337f76c0b
Rodrigo Martin Diaz <rodrigo.m.diaz@hotmail.com>
2022-01-27 21:30:15 -0300

    se agregan comentarios

--- JS/globals.js
+++ JS/globals.js
@@ -3,7 +3,7 @@
 let total = 0; // Se suma en esta variable todos los valores totales de las compras realizadas
 
 let listaProductos; //lista de productos disponibles la cual se va a llenar con los datos obtenidos de productos.json
-let localesDisponibles;
+let localesDisponibles; // lista de locales disponible para retirar , se llena con los datos obtenidos desde json
 
 const detalleDeCompra = []; //Muestra el detalle de todos los productos comprados
 
